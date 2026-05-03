4f3cf166cc635e7bf1ab077a877d3a64c6f9f666
Ian de Rijk <ian.derijk@gmail.com>
2026-05-03 15:00:28 +0100

    changes

--- src/python_setup/setup.py
+++ src/python_setup/setup.py
@@ -1,8 +1,8 @@
 import argparse
-from .setup_uv import setup_uv
-from .setup_prek import setup_prek
-from .setup_dirs import create_project_directories
-from .setup_makefile import create_makefile
+from setup_uv import setup_uv
+from setup_prek import setup_prek
+from setup_dirs import create_project_directories
+from setup_makefile import create_makefile
 
 
 def main() -> None:
